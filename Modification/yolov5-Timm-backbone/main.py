c1be3bb43e3855a3be201754ee9e32c19ea8e07f
东瓜酱 <550502423@163.com>
2023-04-23 17:41:59 +0800

    4.23
    change  the  structure

---
+++ Modification/yolov5-Timm-backbone/main.py
@@ -0,0 +1,23 @@
+
+import torch, timm
+from thop import clever_format, profile
+
+print(timm.list_models())
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+dummy_input = torch.randn(1, 3, 640, 640).to(device)
+
+# model = timm.create_model('edgenext_small', pretrained=False, features_only=True)
+model = timm.create_model('efficientnet_b8', pretrained=False, features_only=True)
+model.to(device)
+model.eval()
+print('--------------------------------')
+print(model.feature_info.channels())
+print('--------------------------------')
+for feature in model(dummy_input):
+    print(feature.size())
+
+flops, params = profile(model.to(device), (dummy_input,), verbose=False)
+flops, params = clever_format([flops * 2, params], "%.3f")
+print('Total FLOPS: %s' % (flops))
+print('Total params: %s' % (params))
